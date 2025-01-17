28d0663ee42a4b5762dd5e867f28a93fd7a7ed76
Hansy <hanshunyao_hansy@163.com>
2025-01-17 19:06:53 +0800

    新增 完成 206. 反转链表

---
+++ 206. 反转链表.js
@@ -0,0 +1,39 @@
+/*
+ * @Author: Hansy hanshunyao_hansy@163.com
+ * @Date: 2025-01-17 19:06:03
+ * @LastEditors: Hansy hanshunyao_hansy@163.com
+ * @LastEditTime: 2025-01-17 19:06:09
+ * @FilePath: \leetcode-problemset\206. 反转链表.js
+ * @Description: 206. 反转链表
+ */
+// 示例 1：
+// 输入：head = [1,2,3,4,5]
+// 输出：[5,4,3,2,1]
+// 示例 2：
+// 输入：head = [1,2]
+// 输出：[2,1]
+// 示例 3：
+// 输入：head = []
+// 输出：[]
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let cur = head;
+  let pre = null;
+  while (cur !== null) {
+    let temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
+  }
+  return pre;
+};
