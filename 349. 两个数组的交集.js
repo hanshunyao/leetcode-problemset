e5ab2ed47f422809c43a66f06e24dd6fa58fb407
Hansy <hanshunyao_hansy@163.com>
2025-01-23 23:40:00 +0800

    新增 完成 349. 两个数组的交集

---
+++ 349. 两个数组的交集.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: Hansy hanshunyao_hansy@163.com
+ * @Date: 2025-01-22 22:15:08
+ * @LastEditors: Hansy hanshunyao_hansy@163.com
+ * @LastEditTime: 2025-01-22 22:15:18
+ * @FilePath: \leetcode-problemset\349. 两个数组的交集.js
+ * @Description: 349. 两个数组的交集
+ */
+// 给定两个数组 nums1 和 nums2 ，返回 它们的 交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+// 示例 1：
+// 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+// 输出：[2]
+// 示例 2：
+// 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// 输出：[9,4]
+// 解释：[4,9] 也是可通过的
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  var newNums1 = new Set(nums1);
+  var newNums2 = new Set(nums2);
+  const result = [];
+  for (item of newNums2) {
+    if (newNums1.has(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
