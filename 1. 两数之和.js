e692f37976f2de241a9c66f9477510e2b08695d3
Hansy <hanshunyao_hansy@163.com>
2025-01-24 20:10:16 +0800

    新增 完成 1. 两数之和

---
+++ 1. 两数之和.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: Hansy hanshunyao_hansy@163.com
+ * @Date: 2025-01-24 20:09:33
+ * @LastEditors: Hansy hanshunyao_hansy@163.com
+ * @LastEditTime: 2025-01-24 20:09:39
+ * @FilePath: \leetcode-problemset\1. 两数之和.js
+ * @Description: 1. 两数之和
+ */
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+// 你可以按任意顺序返回答案。
+// 示例 1：
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 示例 2：
+// 输入：nums = [3,2,4], target = 6
+// 输出：[1,2]
+// 示例 3：
+// 输入：nums = [3,3], target = 6
+// 输出：[0,1]
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map1 = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map1.has(target - nums[i])) {
+      return [i, map1.get(target - nums[i])];
+    } else {
+      map1.set(nums[i], i);
+    }
+  }
+};
