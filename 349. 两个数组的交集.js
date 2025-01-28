3fa63c420d4353e641d4d20d50c120058486bc55
Hansy <hanshunyao_hansy@163.com>
2025-01-28 22:58:26 +0800

    新增 双指针写法

--- 349. 两个数组的交集.js
+++ 349. 两个数组的交集.js
@@ -2,7 +2,7 @@
  * @Author: Hansy hanshunyao_hansy@163.com
  * @Date: 2025-01-22 22:15:08
  * @LastEditors: Hansy hanshunyao_hansy@163.com
- * @LastEditTime: 2025-01-22 22:15:18
+ * @LastEditTime: 2025-01-28 22:58:13
  * @FilePath: \leetcode-problemset\349. 两个数组的交集.js
  * @Description: 349. 两个数组的交集
  */
@@ -29,4 +29,33 @@ var intersection = function (nums1, nums2) {
     }
   }
   return result;
+};
+var intersection = function (nums1, nums2) {
+  nums1.sort((x, y) => x - y);
+  nums2.sort((x, y) => x - y);
+  const length1 = nums1.length,
+    length2 = nums2.length;
+  let index1 = 0,
+    index2 = 0;
+  const intersection = [];
+  while (index1 < length1 && index2 < length2) {
+    const num1 = nums1[index1],
+      num2 = nums2[index2];
+    if (num1 === num2) {
+      // 保证加入元素的唯一性
+      if (
+        !intersection.length ||
+        num1 !== intersection[intersection.length - 1]
+      ) {
+        intersection.push(num1);
+      }
+      index1++;
+      index2++;
+    } else if (num1 < num2) {
+      index1++;
+    } else {
+      index2++;
+    }
+  }
+  return intersection;
 };
