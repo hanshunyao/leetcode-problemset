f34682c0b25f05b7bb1b3a223cc69907759b03b4
Hansy <hanshunyao_hansy@163.com>
2025-02-15 19:54:30 +0800

    新增 开始 347. 前 K 个高频元素 题目

---
+++ 347. 前 K 个高频元素.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Hansy hanshunyao_hansy@163.com
+ * @Date: 2025-02-15 19:53:22
+ * @LastEditors: Hansy hanshunyao_hansy@163.com
+ * @LastEditTime: 2025-02-15 19:53:22
+ * @FilePath: \leetcode-problemset\347. 前 K 个高频元素.js
+ * @Description: 347. 前 K 个高频元素
+ */
+// 示例 1:
+// 输入: nums = [1,1,1,2,2,3], k = 2
+// 输出: [1,2]
+// 示例 2:
+// 输入: nums = [1], k = 1
+// 输出: [1]
+// 提示：
+// 1 <= nums.length <= 105
+// k 的取值范围是 [1, 数组中不相同的元素的个数]
+// 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的
