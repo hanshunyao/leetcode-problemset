8aaba9132e31060042f702131de8e39e65f85faf
Hansy <hanshunyao_hansy@163.com>
2025-02-17 23:06:47 +0800

    重写 使用递归重新完成 144. 二叉树的前序遍历

--- 144. 二叉树的前序遍历.js
+++ 144. 二叉树的前序遍历.js
@@ -2,7 +2,7 @@
  * @Author: Hansy hanshunyao_hansy@163.com
  * @Date: 2025-02-16 19:41:10
  * @LastEditors: Hansy hanshunyao_hansy@163.com
- * @LastEditTime: 2025-02-16 19:43:25
+ * @LastEditTime: 2025-02-17 23:06:27
  * @FilePath: \leetcode-problemset\144. 二叉树的前序遍历.js
  * @Description: 144. 二叉树的前序遍历
  */
@@ -31,18 +31,9 @@
  * @return {number[]}
  */
 var preorderTraversal = function (root, res = []) {
-  const stack = [];
-  if (root) stack.push(root);
-  while (stack.length) {
-    const node = stack.pop();
-    if (!node) {
-      res.push(stack.pop().val);
-      continue;
-    }
-    if (node.right) stack.push(node.right); // 右
-    if (node.left) stack.push(node.left); // 左
-    stack.push(node); // 中
-    stack.push(null);
-  }
+  if (!root) return res;
+  res.push(root.val);
+  preorderTraversal(root.left, res);
+  preorderTraversal(root.right, res);
   return res;
 };
