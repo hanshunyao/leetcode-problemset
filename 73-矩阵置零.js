50bbeb6d26fa8d8a1d575b982679ea40aa2f4652
Hansy <hanshunyao_hansy@163.com>
2025-04-30 21:32:53 +0800

    新增 完成 73. 矩阵置零

---
+++ 73-矩阵置零.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: Hansy hanshunyao_hansy@163.com
+ * @Date: 2025-01-19 22:57:48
+ * @LastEditors: Hansy hanshunyao_hansy@163.com
+ * @LastEditTime: 2025-04-23 00:30:23
+ * @FilePath: \leetcode-problemset\73. 矩阵置零.js
+ * @Description: 73. 矩阵置零
+ */
+// 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+// 示例 1：
+// 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// 输出：[[1,0,1],[0,0,0],[1,0,1]]
+// 示例 2：
+// 输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// 输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+var setZeroes = function (matrix) {
+  const m = matrix.length, n = matrix[0].length;
+  const row = new Array(m).fill(false);
+  const col = new Array(n).fill(false);
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        row[i] = col[j] = true;
+      }
+    }
+  }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (row[i] || col[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+};
